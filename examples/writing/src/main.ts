947eaf53a5b0607c8f15bf5d970901424ca96133
ZachQin <qzkmas@gmail.com>
2023-08-03 00:06:11 +0800

    add writing examples for clarification

---
+++ examples/writing/src/main.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.join(__dirname, "../../../.env") });
+
+import { createLanguageModel, TypeChatLanguageModel } from 'checkmate';
+import { createCheckMate, Checkmate } from 'checkmate';
+
+// Create executor and verifier
+const executor: TypeChatLanguageModel = createLanguageModel(process.env);
+const verifier: TypeChatLanguageModel = createLanguageModel(process.env);
+
+// Create Checkmate instance
+const checkmate: Checkmate = createCheckMate(executor, verifier);
+checkmate.debug = true;
+
+// Define task description
+const taskDescription = 'Write an introduction for a blog post about the importance of digital privacy.';
+
+// Run Checkmate
+async function main(): Promise<void> {
+  const result = await checkmate.perform(taskDescription);
+  console.log('Final result:', result);
+}
+
+main();
